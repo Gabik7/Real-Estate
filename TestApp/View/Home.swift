7cfef46875e30af4c7afec33f9f15f3c40ff5dba
Gabik7 <gabi.falis@gmail.com>
2021-03-28 17:13:46 +0200

    Added Bottom Bar Navigation

--- TestApp/View/Home.swift
+++ TestApp/View/Home.swift
@@ -1,13 +1,65 @@
 import SwiftUI
 
 struct Home: View {
+    @State var show = false
+    
     @AppStorage("log_status") var log_Status = false
     
+    @State var selectedTab = "house.fill"
+    
+    init() {
+        UITabBar.appearance().isHidden = true;
+    }
+    
+    var body: some View {
+        TabView(selection: $selectedTab) {
+            
+            HomeView(show: $show)
+                .tag("house.fill")
+            
+            Text("Location")
+                .tag("location.fill")
+            
+            Text("Favorites")
+                .tag("heart.fill")
+            
+            Text("User")
+                .tag("person.fill")
+        }.overlay(
+            VStack(spacing: 12) {
+                Divider()
+                    .padding(.horizontal, -15)
+                
+                HStack(spacing: 0) {
+                    TabBarButton(image: "house.fill", selectedTab: $selectedTab)
+                        .frame(maxWidth: .infinity)
+                    
+                    TabBarButton(image: "location.fill", selectedTab: $selectedTab)
+                        .frame(maxWidth: .infinity)
+                    
+                    TabBarButton(image: "heart.fill", selectedTab: $selectedTab)
+                        .frame(maxWidth: .infinity)
+                    
+                    TabBarButton(image: "person.fill", selectedTab: $selectedTab)
+                        .frame(maxWidth: .infinity)
+                }
+            }
+            .padding(.horizontal)
+            .padding(.bottom, edges?.bottom ?? 15)
+            .background(Color(#colorLiteral(red: 0.9999018312, green: 1, blue: 0.9998798966, alpha: 1)))
+            .opacity(show ? 0 : 1)
+            , alignment: .bottom)
+        .ignoresSafeArea()
+    }
+}
+
+struct HomeView: View {
     @State var selected = tabs[0]
     @Namespace var animation
     
-    @State var show = false
     @State var selectedProperty: Property = properties[0]
+    
+    @Binding var show: Bool
     
     var body: some View {
         ZStack {
@@ -91,6 +143,21 @@ struct Home: View {
     }
 }
 
+struct TabBarButton: View {
+    var image: String
+    @Binding var selectedTab: String
+    
+    var body: some View {
+        Button(action: {
+            selectedTab = image
+        }, label: {
+            Image(systemName: image)
+                .font(.system(size: 17))
+                .foregroundColor(selectedTab == image ? Color(#colorLiteral(red: 0.08087431639, green: 0.2188317478, blue: 0.5864536762, alpha: 1)) : Color(#colorLiteral(red: 0.8195264935, green: 0.8196648955, blue: 0.8195083141, alpha: 1)))
+        })
+    }
+}
+
 var tabs = ["All", "Residential", "Commercial", "Rural"]
 
 struct HomeView_Previews: PreviewProvider {
@@ -98,3 +165,5 @@ struct HomeView_Previews: PreviewProvider {
         Home()
     }
 }
+
+var edges = UIApplication.shared.windows.first?.safeAreaInsets
